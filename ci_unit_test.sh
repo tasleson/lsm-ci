860379971bbd1791f261105f3cc11b7ca69ac1f1
Tony Asleson <tasleson@redhat.com>
2016-09-02 14:23:17 -0500

    Add ci_unit_test.sh
    Signed-off-by: Tony Asleson <tasleson@redhat.com>

---
+++ ci_unit_test.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+
+# Grab the specified git source tree or rpm (when functionality added),
+# build it and run the specified plugin with the supplied uri and password
+
+if [ "$#" -ne 5 ]; then
+	echo "syntax: ci_unit_test.sh [git|rpm] path/repo ver/branch <array uri> <array password>"
+	exit 1
+fi
+
+what=$1
+loc=$2
+ver=$3
+uri=$4
+pw=$5
+
+# The working directory for this script
+base_dir="/tmp/lsm_ci/src/$RANDOM"
+src_dir="${base_dir}/libstoragemgmt"
+build_dir="${base_dir}/build"
+
+# The directory the functions in the source tree are going to use
+run_dir="/tmp/lsm_ci/run"
+run_dir_rand="${run_dir}/$RANDOM"
+
+function cleanup
+{
+        if [ -e  "${base_dir}" ]
+        then
+		    echo "Deleting ${base_dir}"
+            rm -rf  "${base_dir}"
+        fi
+
+        if [ -e  "${run_dir_rand}" ]
+        then
+		    echo "Deleting ${run_dir_rand}"
+            rm -rf  "${run_dir_rand}"
+        fi
+}
+
+function good
+{
+	# test_include.sh has this function, but we need to run a number of
+	# commands before we can fetch the source to utilize it
+	echo "executing: $@"
+    eval "$@"
+    local ec=$?
+    if [ ${ec} -ne 0 ]; then
+		echo "Fail exit[${ec}]: $1"
+		exit 1
+	fi
+}
+
+function run_test
+{
+    good ./autogen.sh
+    good ./configure $1
+    good make
+
+    # Source in the helpers functions that are included with the source tree
+    good source "${src_dir}/test/test_include.sh"
+
+    # Install, start lsmd and run the plugin unit test and clean-up when done
+    lsm_test_base_install "${run_dir_rand}" "${src_dir}" "${src_dir}" \
+        "${LSM_TEST_INSTALL_ALL_PLUGINS}"
+
+    lsm_test_lsmd_start ${LSM_TEST_WITHOUT_MEM_CHECK}
+
+    # This will test python2 everything
+    lsm_test_plugin_test_run "${uri}"  "${pw}"
+
+    lsm_test_cleanup
+}
+
+# When we exit we are going to clean up what we created so we don't fill
+# the FS up, comment this out if we need the files around to debug
+# trap cleanup EXIT
+
+# Make the needed directories, but don't make the destination directory for
+# argument 1 for lsm_test_base_install as it wants to create it
+good mkdir -p "${base_dir}"
+good mkdir -p "${build_dir}"
+good mkdir -p "${run_dir}"
+
+# Start in a consistent spot
+good cd "${base_dir}"
+
+# Fetch the code or rpm, or make sure rpm version is installed
+if [ "${what}" = "git" ] ; then
+	good git clone "${loc}" --branch "${ver}"
+else
+	echo "Testing with RPMs not completed yet!"
+	exit 1
+fi
+
+good cd "${src_dir}"
+
+# Do basic py2 test
+run_test ""
+
+# Lets see if python3 is available in configure
+./configure --help | grep "python3"
+rc=$?
+if [ ${rc} -eq 0 ]; then
+    echo "Re-running plugin test with python3 interpreter"
+
+    # Start at a consistent state
+    good git clean -fxd
+
+    run_test --with-python3
+else
+    echo "No python3 support found skipping..."
+fi
+
+exit 0
