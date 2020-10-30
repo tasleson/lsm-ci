89050ee9d1420b2e4737bdf1fc0416415a3b24ea
Tony Asleson <tasleson@redhat.com>
2020-10-30 13:46:27 -0500

    crash_serve: Add addl. tests
    Test invalid and valid certificates.
    
    Signed-off-by: Tony Asleson <tasleson@redhat.com>

--- testing/crash_serve.py
+++ testing/crash_serve.py
@@ -7,8 +7,8 @@ import time
 import os
 
 
-PORT_NUM_CONTROL = os.getenv('PORT_NUM_CONTROL', "43301")
-PORT_NUM_PEER_SSL = os.getenv('PORT_NUM_PEER_SSL', "443")
+PORT_NUM_CONTROL = int(os.getenv('PORT_NUM_CONTROL', "43301"))
+PORT_NUM_PEER_SSL = int(os.getenv('PORT_NUM_PEER_SSL', "443"))
 IP_ADDRESS = os.getenv("IP_ADDRESS", "127.0.0.1")
 
 
@@ -26,6 +26,54 @@ def failing_ssl():
         ssl_sock.close()
     except Exception as e:
         print("failing_ssl: %s" % str(e))
+
+    time.sleep(0.2)
+
+
+def invalid_ssl_cert():
+    """
+    Use a cert, just not the correct one.
+    :return:  None
+    """
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("Created socket!")
+        ssl_sock = ssl.wrap_socket(s,
+                                   ca_certs="wrong_server_cert.pem",
+                                   cert_reqs=ssl.CERT_REQUIRED,
+                                   certfile="wrong_client_cert.pem",
+                                   keyfile="wrong_client_key.pem")
+        print("Have ssl_sock!")
+        ssl_sock.connect((IP_ADDRESS, PORT_NUM_PEER_SSL))
+        print("invalid_ssl_cert: connected")
+        time.sleep(3)
+        ssl_sock.close()
+    except Exception as e:
+        print("invalid_ssl_cert: %s" % str(e))
+
+    time.sleep(0.2)
+
+
+def valid_ssl_cert():
+    """
+    Use the correct cert
+    :return:  None
+    """
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        print("Created socket!")
+        ssl_sock = ssl.wrap_socket(s,
+                                   ca_certs="server_cert.pem",
+                                   cert_reqs=ssl.CERT_REQUIRED,
+                                   certfile="client_cert.pem",
+                                   keyfile="client_key.pem")
+        print("Have ssl_sock!")
+        ssl_sock.connect((IP_ADDRESS, PORT_NUM_PEER_SSL))
+        print("valid_ssl_cert: connected")
+        time.sleep(5)
+        ssl_sock.close()
+    except Exception as e:
+        print("valid_ssl_cert: %s" % str(e))
 
     time.sleep(0.2)
 
@@ -54,3 +102,5 @@ def open_write():
 while True:
     failing_ssl()
     open_write()
+    invalid_ssl_cert()
+    valid_ssl_cert()
